e117634e1a6debee31b23931bf2a67f31f76bb89
Bibhuti Pradhan <bibhutipradhan2222@gmail.com>
2026-07-16 11:46:11 +0530

    Added Render port support

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const startScheduler = require("./services/schedulerService");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
